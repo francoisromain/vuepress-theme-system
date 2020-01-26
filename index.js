7870414d9d97ab46e02879e7e0d7d137756e5a44
Francois Romain <francoisromain@gmail.com>
2020-01-26 14:12:04 +0100

    fix postcss plugins import

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-module.exports = {
-  globalLayout: '/layouts/Layout.vue',
-  postcss: {
-    plugins: [
-      require('postcss-normalize'),
-      require('postcss-import'),
-      require('postcss-typescale'),
-      require('postcss-font-family-system-ui'),
-      require('pixrem')({
-        html: false
-      }),
-      require('postcss-grid-system'),
-      require('postcss-grid-fluid'),
-      require('postcss-button'),
-      require('postcss-inline-svg'),
-      require('postcss-color-mod-function'),
-      require('postcss-preset-env')({
-        stage: 0
-      })
-    ]
-  }
-}
+module.exports = (options, ctx) =>
+  Object.assign(ctx, {
+    postcss: {
+      plugins: [
+        require('postcss-normalize'),
+        require('postcss-import'),
+        require('postcss-typescale'),
+        require('postcss-font-family-system-ui'),
+        require('pixrem')({
+          html: false
+        }),
+        require('postcss-grid-system'),
+        require('postcss-grid-fluid'),
+        require('postcss-button'),
+        require('postcss-inline-svg'),
+        require('postcss-color-mod-function'),
+        require('postcss-preset-env')({
+          stage: 0
+        })
+      ]
+    }
+  })
