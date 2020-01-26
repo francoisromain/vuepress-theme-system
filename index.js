ac3bb96a63f7a62b69225ff0902a71052f155594
Francois Romain <francoisromain@gmail.com>
2020-01-26 18:02:25 +0100

    fix postcss config

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 module.exports = {
   chainWebpack(config, isServer) {
-    config.merge({
-      postcss: {
-        plugins: [
-          require('postcss-normalize'),
-          require('postcss-import'),
-          require('postcss-typescale'),
-          require('postcss-font-family-system-ui'),
-          require('pixrem')({
-            html: false
-          }),
-          require('postcss-grid-system'),
-          require('postcss-grid-fluid'),
-          require('postcss-button'),
-          require('postcss-inline-svg'),
-          require('postcss-color-mod-function'),
-          require('postcss-preset-env')({
-            stage: 0
-          })
-        ]
-      }
-    })
+    console.log(config)
+    // config.merge({
+    //   postcss: {
+    //     plugins: [
+    //       require('postcss-normalize'),
+    //       require('postcss-import'),
+    //       require('postcss-typescale'),
+    //       require('postcss-font-family-system-ui'),
+    //       require('pixrem')({
+    //         html: false
+    //       }),
+    //       require('postcss-grid-system'),
+    //       require('postcss-grid-fluid'),
+    //       require('postcss-button'),
+    //       require('postcss-inline-svg'),
+    //       require('postcss-color-mod-function'),
+    //       require('postcss-preset-env')({
+    //         stage: 0
+    //       })
+    //     ]
+    //   }
+    // })
   }
 }
