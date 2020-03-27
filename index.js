29cff026bcf364ae014a2c87cf6ef43db7ca3759
Francois Romain <francoisromain@gmail.com>
2020-03-27 10:18:13 +0100

    update dependencies

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ module.exports = {
       require('postcss-import'),
       require('postcss-typescale'),
       require('postcss-font-family-system-ui'),
-      require('pixrem')({
-        html: false
-      }),
+      require('pixrem')({ html: false }),
       require('postcss-grid-system'),
       require('postcss-grid-fluid'),
       require('postcss-button'),
       require('postcss-inline-svg'),
       require('postcss-color-mod-function'),
-      require('postcss-preset-env')({
-        stage: 0
-      })
+      require('postcss-preset-env')({ stage: 0 })
     ]
   }
 }
